e69853ce8d558a0f04c049d5e24d6f2ae0618927
Mattias.Astrom <mattias.astrom@hotmail.com>
2024-09-17 09:13:48 +0200

    Add project files.

---
+++ GrundernaOOP/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GrundernaOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle firstCircle = new(5);
+            Circle secondCircle = new(6);
+
+            Console.WriteLine($"Area of the first circle with a radius of {firstCircle.GetRadius().ToString()} and the areas is: {firstCircle.GetArea().ToString()}");
+            Console.WriteLine($"Area of the first circle with a radius of {secondCircle.GetRadius().ToString()} and the areas is: {secondCircle.GetArea().ToString()}");
+        }
+    }
+
+    class Circle
+    {
+        int _radius;
+        public Circle(int radius)
+        {
+            _radius = radius;
+        }
+        public int GetRadius()
+        {
+            return _radius;
+        }
+        public int  GetArea()
+        {
+            return (int)(_radius * _radius * Math.PI);
+        }
+    }
+}
